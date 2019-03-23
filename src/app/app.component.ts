dbbd71258477b828e75dcd03b19364c9f7fc4091
unknown <manzoor1996alam@gmail.com>
2019-03-24 00:06:21 +0530

    FCM in ionic 4 by using import { FCM } from '@ionic-native/fcm/ngx';

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FCM } from '@ionic-native/fcm/ngx'
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public fcm:FCM,
+    public afs:AngularFirestore
   ) {
     this.initializeApp();
   }
@@ -21,6 +26,35 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      
+      //fcm code start 
+      this.fcm.getToken().then((token)=>{
+        localStorage.setItem("token",token);
+        
+        this.afs.collection("Devices").add({
+           token:token
+        })
+        alert(token)
+
+    }, (err)=>{
+          alert(JSON.stringify(err));
+    })
+
+    this.fcm.onNotification().subscribe((data)=>{
+      if(data.wasTapped){
+         
+      }
+      else{
+         alert(data.message);
+      }
+    })
+
+
+    this.fcm.onTokenRefresh().subscribe((token)=>{
+      localStorage.setItem("token",token)
+    })
+ //fcm code end
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,38 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FCM } from '@ionic-native/fcm/ngx';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const config = {
+  //ionicdemo
+  apiKey: "AIzaSyD1Hdh_xG8KZPiVnpw6AeF3EqyEBX7LyTE",
+  authDomain: "ionicdemo-33e24.firebaseapp.com",
+  databaseURL: "https://ionicdemo-33e24.firebaseio.com",
+  projectId: "ionicdemo-33e24",
+  storageBucket: "ionicdemo-33e24.appspot.com",
+  messagingSenderId: "51814017590"
+  };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+     IonicModule.forRoot(), 
+     AppRoutingModule,
+      AngularFireAuthModule,
+      AngularFireModule.initializeApp(config),
+      AngularFirestoreModule
+    ],
   providers: [
+    FCM,
     StatusBar,
     SplashScreen,
+    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
